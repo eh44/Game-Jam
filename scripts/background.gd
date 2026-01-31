3e1e1550029839ccd9ce6bf301d6f3cf5f97bf66
Gustavo Tavo Huerta <gustavohuerta96@gmail.com>
2026-01-30 17:04:23 -0700

    sorejgoj

---
+++ scripts/background.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var OFFSET = 5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
